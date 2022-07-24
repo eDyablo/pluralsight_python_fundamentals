1c50a1060ff4276315a637c7bed51c096c34e22e
ed yablonsky <eyablonsky@gmail.com>
2022-07-23 18:47:33 -0700

    add planets

--- planets/main.py
+++ planets/main.py
@@ -4,6 +4,34 @@ from planet import Planet
 
 
 def main():
+    mercury = Planet(
+        "Mercury",
+        radius_metres=2439.7e3,
+        mass_kilograms=3.3022e23,
+        orbital_period_seconds=7.60052e6,
+        surface_temperature_kelvin=340,
+    )
+    venus = Planet(
+        "Venus",
+        radius_metres=6051.8e3,
+        mass_kilograms=4.8676e24,
+        orbital_period_seconds=1.94142e7,
+        surface_temperature_kelvin=737,
+    )
+    earth = Planet(
+        "Earth",
+        radius_metres=6371.0e3,
+        mass_kilograms=5.972e24,
+        orbital_period_seconds=3.15581e7,
+        surface_temperature_kelvin=288,
+    )
+    mars = Planet(
+        "Mars",
+        radius_metres=3389.5e3,
+        mass_kilograms=6.4185e23,
+        orbital_period_seconds=5.93543e7,
+        surface_temperature_kelvin=210,
+    )
     pluto = Planet(
         "Pluto",
         radius_metres=1184e3,
@@ -11,6 +39,8 @@ def main():
         orbital_period_seconds=7816012992,
         surface_temperature_kelvin=55,
     )
+
+    return mercury, venus, earth, mars, pluto
 
 
 if __name__ == "__main__":
