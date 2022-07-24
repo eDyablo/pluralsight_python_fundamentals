404fe20ae1819e5e59b2424f5d174fb4657bc779
ed yablonsky <eyablonsky@gmail.com>
2022-07-23 18:40:23 -0700

    define and use positive descriptor

---
+++ planets/descriptor.py
@@ -0,0 +1,17 @@
+from weakref import WeakKeyDictionary
+
+
+class Positive:
+    def __init__(self):
+        self._instance_data = WeakKeyDictionary()
+
+    def __get__(self, instance, _):
+        return self._instance_data[instance]
+
+    def __set__(self, instance, value):
+        if value <= 0:
+            raise ValueError("Value {} is not positive".format(value))
+        self._instance_data[instance] = value
+
+    def __delete__(self, _):
+        raise AttributeError("Cannot delete attribute")
--- planets/planet.py
+++ planets/planet.py
@@ -1,3 +1,6 @@
+from descriptor import Positive
+
+
 class Planet:
     def __init__(
         self,
@@ -23,56 +26,7 @@ class Planet:
             raise ValueError("Cannot set empty Planet.name")
         self._name = value
 
-    def _get_radius_metres(self):
-        return self._radius_metres
-
-    def _set_radius_metres(self, value):
-        if value <= 0:
-            raise ValueError(
-                "Planet.radius_metres value {} is not positive".format(value)
-            )
-        self._radius_metres = value
-
-    radius_metres = property(fget=_get_radius_metres, fset=_set_radius_metres)
-
-    def _get_mass_kilograms(self):
-        return self._mass_kilograms
-
-    def _set_mass_kilograms(self, value):
-        if value <= 0:
-            raise ValueError(
-                "Planet.mass_kilograms value {} is not positive".format(value)
-            )
-        self._mass_kilograms = value
-
-    mass_kilograms = property(fget=_get_mass_kilograms, fset=_set_mass_kilograms)
-
-    def _get_orbital_period_seconds(self):
-        return self._orbital_period_seconds
-
-    def _set_orbital_period_seconds(self, value):
-        if value <= 0:
-            raise ValueError(
-                "Planet.orbital_period_seconds value {} is not positive".format(value)
-            )
-        self._orbital_period_seconds = value
-
-    orbital_period_seconds = property(
-        fget=_get_orbital_period_seconds, fset=_set_orbital_period_seconds
-    )
-
-    def _get_surface_temperature_kelvin(self):
-        return self._surface_temperature_kelvin
-
-    def _set_surface_temperature_kelvin(self, value):
-        if value <= 0:
-            raise ValueError(
-                "Planet.surface_temperature_kelvin value {} is not positive".format(
-                    value
-                )
-            )
-        self._surface_temperature_kelvin = value
-
-    surface_temperature_kelvin = property(
-        fget=_get_surface_temperature_kelvin, fset=_set_surface_temperature_kelvin
-    )
+    radius_metres = Positive()
+    mass_kilograms = Positive()
+    orbital_period_seconds = Positive()
+    surface_temperature_kelvin = Positive()
